9a840fb01fac060177e185a7356b0396926904e1
Julien Richard <45456851+jurichar@users.noreply.github.com>
2023-09-13 16:40:09 +0200

    add loading and users pages and user pages

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,15 @@
-export default async function users() {
+import UserCard from "@/components/UserCard/UserCard";
+import { prisma } from "@/lib/prisma";
+import styles from "./page.module.css";
+
+export default async function Users() {
+  const users = await prisma.user.findMany();
+
   return (
-    <div>
-      <h1>Users</h1>
-      <p>This is the blog page</p>
+    <div className={styles.grid}>
+      {users.map((user) => {
+        return <UserCard key={user.id} {...user} />;
+      })}
     </div>
   );
 }
