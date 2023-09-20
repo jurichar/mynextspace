6b4f1b98ca51d20282eb87161977eb4aa68d4e1f
Julien Richard <45456851+jurichar@users.noreply.github.com>
2023-09-20 03:18:30 +0200

    add database migration

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,5 @@
+import { prisma } from '@/lib/prisma';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import NextAuth from 'next-auth';
 import type { NextAuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
