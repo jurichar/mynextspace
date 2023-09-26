e1a0be8735e23d8ffc71d103b8a4a8a0327ab642
Julien Richard <45456851+jurichar@users.noreply.github.com>
2023-09-26 18:12:56 +0200

    fix typo

--- app/api/follow/route.ts
+++ app/api/follow/route.ts
@@ -1,16 +1,16 @@
-import { NextRequest, NextResponse } from "next/server";
-import { getServerSession } from "next-auth";
 import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
 import { authOptions } from "../auth/[...nextauth]/route";
 
-export async function POST(req: Request){
+export async function POST(req: Request) {
     const session = await getServerSession(authOptions);
     const currentUserEmail = session?.user?.email!;
     const { targetUserId } = await req.json();
 
     const currentUserId = await prisma.user
-    .findUnique({where: {email: currentUserEmail}})
-    .then((user) => user?.id!);
+        .findUnique({ where: { email: currentUserEmail } })
+        .then((user) => user?.id!);
 
     const record = await prisma.follows.create({
         data: {
@@ -22,22 +22,22 @@ export async function POST(req: Request){
     return NextResponse.json(record);
 }
 
-export async function DELETE(req: NextRequest){
-   const session = await getServerSession(authOptions);
-   const currentUserEmail = session?.user?.email!;
-   const targetUserId = req.nextUrl.searchParams.get('targetUserId');
-  
+export async function DELETE(req: NextRequest) {
+    const session = await getServerSession(authOptions);
+    const currentUserEmail = session?.user?.email!;
+    const targetUserId = req.nextUrl.searchParams.get("targetUserId");
+
     const currentUserId = await prisma.user
-    .findUnique({where: {email: currentUserEmail}})
-    .then((user) => user?.id!);
+        .findUnique({ where: { email: currentUserEmail } })
+        .then((user) => user?.id!);
 
     const record = await prisma.follows.delete({
         where: {
             followerId_followingId: {
                 followerId: currentUserId,
                 followingId: targetUserId!,
-            }
-        }
+            },
+        },
     });
 
     return NextResponse.json(record);
